3c9d221e2d185124700131da3388e8c3ab2daaee
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-09-19 11:13:00 -0300

    Update procedimento.js

--- Javascript/aula1 - Introdução/scripts/procedimento.js
+++ Javascript/aula1 - Introdução/scripts/procedimento.js
@@ -1,3 +1,11 @@
 function procedimento(){
-    alert("IHULL")
+    let nome = prompt("Seja bem vindo(a), qual é o seu nome?")
+    // alert(nome+" coloque o primeiro número a ser somado")
+    let n1 = Number(prompt(`${nome} coloque o primeiro numero a ser somado`))
+    let n2 = parseInt(prompt(`${nome} coloque o segundo numero a ser somado`))
+
+    // Number(), parseInt(), parseFloat() - converte string para numero
+    let resultado = n1 + n2
+
+    alert(`A soma dos números é de ${resultado}`)
 }
