45da08ecaa3bff21357e7e7712084eac0e5d01f7
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-09-20 08:54:59 -0300

    aula 1 correção

--- Javascript/aula1 - Introdução/scripts/procedimento.js
+++ Javascript/aula1 - Introdução/scripts/procedimento.js
@@ -9,3 +9,25 @@ function procedimento(){
 
     alert(`A soma dos números é de ${resultado}`)
 }
+
+// atividades
+// 1-calcule conta, faça uma aplicação js que pegue o número de clientes em uma mesa, o valor total da conta e após isso divida a conta de forma igual a todos os clientes
+// input = nClientes, vTotal
+// output = resultadoDaConta
+function ex1Correcao(){
+    let nClientes = Number(prompt("Numero de clientes:"))
+    let vTotal = Number(prompt("Valor total da mesa R$:"))
+
+    let resultadoDaConta = vTotal / nClientes
+
+    alert(`O valor para cada cliente é de : ${resultadoDaConta}`)
+}
+
+// 2-Sucessor e antecessor, faça uma aplicação que colete um número digitado pelo usuário e logo em seguida mostre em ordem: o numero anterior a ele, o próprio número escolhido e o número sucessor a ele
+// input = numero
+// output = sucessor, antecessor, numero
+function ex2Correcao(){
+    let numero = Number(prompt("Digite um numero"))
+
+    alert(`O antecessor é ${numero - 1}, o próprio numero é ${numero} e o sucessor é ${numero + 1}`)
+}
